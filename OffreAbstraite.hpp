8ecff6964fa742d40b9a0f8080d97c507181eb19
Wertubald <101939619+Wertubald@users.noreply.github.com>
2024-11-29 13:43:55 -0500

    Feat: Offre Abstraite

---
+++ OffreAbstraite.hpp
@@ -0,0 +1,29 @@
+// OffreAbstraite.h
+///////////////////////////////////////////////////////////
+//  OffreAbstraite.h
+//  Abstract class definition for Offre
+//  Created on:      17-Nov-2024 2:28:22 PM
+//  Original author: Alex
+///////////////////////////////////////////////////////////
+
+#ifndef OFFRE_ABSTRAITE_HPP
+#define OFFRE_ABSTRAITE_HPP
+
+#include <string>
+#include <memory>
+#include "Devise.hpp"
+
+namespace std {
+    class OffreAbstraite {
+    public:
+        OffreAbstraite() {}
+        virtual ~OffreAbstraite() {}
+
+        virtual string obtenirNom() const = 0;
+        virtual string obtenirType() const = 0;
+        virtual shared_ptr<Devise> obtenirDevise() const = 0;
+        virtual double obtenirPrix() const = 0;
+        virtual double calculerPrixTotal(const Devise& autreDevise, double taxe = 1.0) const = 0;
+    };
+}
+#endif // OFFRE_ABSTRAITE_HPP
